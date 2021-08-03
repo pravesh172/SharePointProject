79520156cf0fd1056e23c789a27872d717a1177c
pravesh172 <55476630+pravesh172@users.noreply.github.com>
2021-08-03 12:01:01 +0530

    Update TestGithub.js

--- TestGithub.js
+++ TestGithub.js
@@ -2,20 +2,6 @@ var app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope, $http) {
 
     $scope.GetData = function () {
-        $http
-            ({
-                method: 'GET',
-                url: _spPageContextInfo.siteAbsoluteUrl + "/_api/web/lists/getByTitle('TestGithubList')/items?$select=Title",
-                header: {
-                    "Accept": "application/json;odata=verbose",
-                    "Access-Control-Allow-Origin": "*"
-                }
-            }).then(function (getdata) {               
-                $scope.myData = getdata.data.value;               
-            },
-                function (error) {
-                    console.log(error);
-                });
+        $scope.Data='MyNameIs Pravesh Kumar'
     }
-    $scope.GetData();
 });
